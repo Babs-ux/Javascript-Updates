192ec7f8810b392e4d41b6e83f2c509e83c971a4
Babari <bdumkalegbara@gmail.com>
2025-06-04 10:10:02 +0100

    Challenge #5

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-//Task to store BMI data of Mark and John in variables, calculate their BMI and log the valuese in console
+//Challenge to calculate the average of 3 scores from 2 gymnastic teams (Dolphins and Koalas) using an arrow function.
 
-const massMark = 78; // kg
-const heightMark = 1.69; // m
-const massJohn = 92; // kg
-const heightJohn = 1.95; // m
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
+'use strict';
 
-//New task: comparing the BMIs of Mark and John and logging the result in console
+// Function to calculate the average score using three scores as the parameters and return the average.
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-if( BMIMark > BMIJohn ) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
-}else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win  🏆 (${avgDolphins} vs ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win 🏆 (${avgKoalas} vs ${avgDolphins})`);
+    } else {
+        console.log('No team wins. Draw!');
+    }
 }
+//For example:
+checkWinner(576, 111);
 
+
+// Test with different scores
+
+// const scoreDolphins = calcAverage(85, 54, 41);
+// const scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
