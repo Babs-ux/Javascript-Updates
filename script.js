4b69a015728c3c34896024142b816980e43cafb5
Babari <bdumkalegbara@gmail.com>
2025-06-17 14:59:31 +0100

    Click event

--- script.js
+++ script.js
@@ -2,29 +2,7 @@
 
 'use strict';
 
-// Function to calculate the average score using three scores as the parameters and return the average.
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-
-const scoreDolphins = calcAverage(44, 23, 71);
-const scoreKoalas = calcAverage(65, 54, 49);
-console.log(scoreDolphins, scoreKoalas);
-
-const checkWinner = function (avgDolphins, avgKoalas) {
-    if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win  🏆 (${avgDolphins} vs ${avgKoalas})`);
-    } else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win 🏆 (${avgKoalas} vs ${avgDolphins})`);
-    } else {
-        console.log('No team wins. Draw!');
-    }
-}
-//For example:
-checkWinner(576, 111);
-
-
-// Test with different scores
-
-// const scoreDolphins = calcAverage(85, 54, 41);
-// const scoreKoalas = calcAverage(23, 34, 27);
-// console.log(scoreDolphins, scoreKoalas);
-// checkWinner(scoreDolphins, scoreKoalas);
+document.querySelector('.check').addEventListener('click', function () {
+    const guess  = document.querySelector('.guess').value;
+    console.log(guess);
+});
