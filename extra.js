b3b34b612c92ed15a1dd4e6d2c3b51cd7c2bbb60
Babari <bdumkalegbara@gmail.com>
2025-06-24 08:58:05 +0100

    First task

--- extra.js
+++ extra.js
@@ -1,11 +1,82 @@
-//Challenge to make a very simple tip calculator for whenever "Steve" goes out to eat with his friends.
+//Challenge to make a football betting application
 
 
-//Created a variable for the bill amount.
-const bill = 275;
 
-//Created a variable for the tip amount based on the bill amount. 
-// If the bill is between 50 and 300, the tip is 15%, otherwise it is 20%.
-const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+'use strict';
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}.`);
+// Data for the football game
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2020',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// one player array for each team (variables 'players1' and 'players2')
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// players1 is Bayern, players2 is Dortmund. The first player of players1 is the goalkeeper
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// Created an array 'allPlayers' containing all players of both teams
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// Created an array 'players1Final' containing all players of players1 and 3 substitutions
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+//Odds for each output
+const { odds: { team1, x: draw, team2 } } = game;
+console.log(team1, draw, team2);
+
+// 
+team1 < team2 && console.log('Team 1 is more likely to win'); // Log if Team 1 has better odds (lower value)
+team1 > team2 && console.log('Team 2 is more likely to win'); // Log if Team 2 has better
+
+// Function to print the goals scored
+const printGoals = function (...players) {
+    console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+//Test Data
+printGoals(...game.scored);
+
+
